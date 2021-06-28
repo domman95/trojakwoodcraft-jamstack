46b81955bcf9446716c28717b8b11160280fc54f
domman95 <dominiklasek95@gmail.com>
2021-06-28 18:08:57 +0200

    typeof window

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,6 +1,13 @@
 import { navigate } from 'gatsby';
 
 export default function Admin() {
+  const isBrowser = typeof window === 'object';
+
+  useEffect(() => {
+    if (!isBrowser) return null;
+    return () => cleanup();
+  }, []);
+
   navigate('https://trojakwoodcraft.admin.datocms.com/', { replace: true });
   return null;
 }
