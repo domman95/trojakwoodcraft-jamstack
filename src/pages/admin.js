4138a4e6bb5d4cc8bc9870147c239f72185b2d89
domman95 <dominiklasek95@gmail.com>
2021-06-28 17:31:12 +0200

    fix: window is not defined

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -2,6 +2,6 @@ import { navigate } from 'gatsby';
 import React from 'react';
 
 export default function AdminPage() {
-  navigate('https://trojakwoodcraft.admin.datocms.com/');
+  navigate('https://trojakwoodcraft.admin.datocms.com/', { replace: true });
   return null;
 }
