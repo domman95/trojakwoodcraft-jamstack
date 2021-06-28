a492231a3e3100b4064178990252282d47d08747
domman95 <dominiklasek95@gmail.com>
2021-06-28 17:37:41 +0200

    deleted admin page

--- src/pages/admin.js
+++
@@ -1,7 +0,0 @@
-import { navigate } from 'gatsby';
-import React from 'react';
-
-export default function AdminPage() {
-  navigate('https://trojakwoodcraft.admin.datocms.com/', { replace: true });
-  return null;
-}
